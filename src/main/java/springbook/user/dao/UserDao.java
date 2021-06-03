79342e9d952ef872f172f63db00d2845d1631442
jujubebat <bbwwpark@naver.com>
2021-06-03 16:31:31 +0900

    refactor: DB 커넥션을 가져오는 로직을 메서드로 분리함

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -10,9 +10,9 @@ import springbook.user.domain.User;
 public class UserDao {
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "spring", "book");
-        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+        Connection c = getConnection();
+        PreparedStatement ps = c
+            .prepareStatement("insert into users(id, name, password) values(?,?,?)");
         ps.setString(1, user.getId());
         ps.setString(2, user.getName());
         ps.setString(3, user.getPassword());
@@ -22,8 +22,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "spring", "book");
+        Connection c = getConnection();
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
         ps.setString(1, id);
         ResultSet rs = ps.executeQuery();
@@ -36,6 +35,13 @@ public class UserDao {
         ps.close();
         c.close();
         return user;
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+            "jdbc:mysql://localhost/springbook", "spring", "book");
+        return c;
     }
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
