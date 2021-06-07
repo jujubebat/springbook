5aea8f909d1f4a5975097b2f9b602bb1e0927866
jujubebat <bbwwpark@naver.com>
2021-06-07 22:14:54 +0900

    refactor: UserDao와 ConnectionMaker 구현 클래스 관계 설정 로직을 DataFactory가 담당하도록 수정

--- src/test/java/springbook/user/dao/UserDaoTest.java
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -7,8 +7,7 @@ class UserDaoTest {
 
     public static void main(String[] args) throws ClassNotFoundException,
         SQLException {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-        UserDao dao = new UserDao(connectionMaker);
+        UserDao dao = new DaoFactory().userDao();
         User user = new User();
         user.setId("whiteship");
         user.setName("백기선");
