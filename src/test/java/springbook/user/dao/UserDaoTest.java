65099d5d8136a365a851bad046f4ff34b5bb9258
jujubebat <bbwwpark@naver.com>
2021-06-09 21:31:06 +0900

    refactor: 애플리케이션 컨텍스트를 아예 사용하지 않도록 리팩터링
    - UserDaoTest는 사실 UserDao 코드가 DAO로서 DB에 정보를 잘 등록하고 잘 가 져오는지만 확인하면 된다. 스프링 컨테이너에서 UserDao가 동작함을 확인하는 일은 UserDaoTest의 기본적인 관심사가 아니다.
    - DI는 객체지향 프로그래밍 스타일이다. 따라서 DI를 위해 컨 테이너가 반드시 필요한 것은 아니다. DI 컨테이너나 프레임워크는 DI를 편하게 적용하 도록 도움을 줄 뿐, 컨테이너가 DI를 가능하게 해주는 것은 아니다.

--- src/test/java/springbook/user/dao/UserDaoTest.java
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -4,22 +4,17 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.sql.SQLException;
+import javax.sql.DataSource;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 import springbook.user.domain.User;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "/test-applicationContext.xml")
 public class UserDaoTest {
 
-    @Autowired
     private UserDao dao;
     private User user1;
     private User user2;
@@ -30,6 +25,10 @@ public class UserDaoTest {
         this.user1 = new User("gyumee", "박성철", "springno1");
         this.user2 = new User("leegw700", "이길원", "springno2");
         this.user3 = new User("bumjin", "박범진", "springno3");
+
+        dao = new UserDao();
+        DataSource dataSource = new SingleConnectionDataSource("jdbc:mysql://localhost/testdb", "spring", "book", true);
+        dao.setDataSource(dataSource);
     }
 
     @Test
