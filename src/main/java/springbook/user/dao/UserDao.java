99dbed482cc89cf16a390d60bb4bad85c85dc11d
jujubebat <bbwwpark@naver.com>
2021-06-09 20:03:12 +0900

    refactor: 동일한 결과를 보장하는 테스트가되도록 리팩터링 - addAndGet 메서드 실행전 user 테이블을 모두 지우는 작업 추가

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -17,7 +17,8 @@ public class UserDao {
 
     public void add(User user) throws ClassNotFoundException, SQLException {
         Connection c = dataSource.getConnection();
-        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+        PreparedStatement ps = c
+            .prepareStatement("insert into users(id, name, password) values(?,?,?)");
         ps.setString(1, user.getId());
         ps.setString(2, user.getName());
         ps.setString(3, user.getPassword());
@@ -41,5 +42,20 @@ public class UserDao {
         c.close();
         return user;
     }
+
+    public void deleteAll() throws SQLException {
+        Connection c = dataSource.getConnection();
+        PreparedStatement ps = c.prepareStatement("delete from users");
+        ps.executeUpdate();
+        ps.close();
+        c.close();
+    }
+
+    public int getCount() throws SQLException { Connection c = dataSource.getConnection();
+        PreparedStatement ps = c.prepareStatement("select count(*) from users");
+        ResultSet rs = ps.executeQuery(); rs.next();
+        int count = rs.getInt(1);
+        rs.close(); ps.close(); c.close();
+        return count; }
 
 }
--- src/test/java/springbook/user/dao/UserDaoTest.java
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertThat;
 
 import java.sql.SQLException;
 import org.junit.Test;
-import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import springbook.user.domain.User;
@@ -16,17 +15,22 @@ public class UserDaoTest {
     public void addAndGet() throws SQLException, ClassNotFoundException {
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         UserDao dao = context.getBean("userDao", UserDao.class);
+
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
         User user = new User();
         user.setId("gyumee");
         user.setName("박성철");
         user.setPassword("springno1");
+
         dao.add(user);
+        assertThat(dao.getCount(), is(1));
+
         User user2 = dao.get(user.getId());
+
         assertThat(user2.getName(), is(user.getName()));
         assertThat(user2.getPassword(), is(user.getPassword()));
     }
 
-    public static void main(String[] args) {
-        JUnitCore.main("springbook.user.dao.UserDaoTest");
-    }
 }
