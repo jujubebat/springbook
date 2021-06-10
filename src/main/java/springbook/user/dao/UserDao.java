62888617db141aa6e792664cb544ace3b65ebe8c
jujubebat <bbwwpark@naver.com>
2021-06-10 17:02:36 +0900

    refactor: 재사용 가능한 콜백을 인스턴스 변수로 분리 - 사용되지 않는 클래스 제거

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -10,6 +10,16 @@ import springbook.user.domain.User;
 
 public class UserDao {
 
+    private RowMapper<User> userMapper = new RowMapper<User>() {
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            User user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+            return user;
+        }
+    };
+
     private JdbcTemplate jdbcTemplate;
 
     public void setDataSource(DataSource dataSource) {
@@ -26,31 +36,11 @@ public class UserDao {
     }
 
     public User get(String id) {
-        return this.jdbcTemplate.queryForObject("select * from users where id = ?",
-            new Object[]{id},
-            new RowMapper<User>() {
-                public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-                    User user = new User();
-                    user.setId(rs.getString("id"));
-                    user.setName(rs.getString("name"));
-                    user.setPassword(rs.getString("password"));
-                    return user;
-                }
-            });
+        return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[]{id}, this.userMapper);
     }
 
     public List<User> getAll() {
-        return this.jdbcTemplate.query("select * from users order by id",
-            new RowMapper<User>() {
-                public User mapRow(ResultSet rs, int rowNum)
-                    throws SQLException {
-                    User user = new User();
-                    user.setId(rs.getString("id"));
-                    user.setName(rs.getString("name"));
-                    user.setPassword(rs.getString("password"));
-                    return user;
-                }
-            });
+        return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
     }
 
     public int getCount() {
