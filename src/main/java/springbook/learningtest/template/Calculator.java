0631d4262fd04bf9a02f5690a29e1119e7cac586
jujubebat <bbwwpark@naver.com>
2021-06-10 16:02:26 +0900

    refactor: 템플릿/콜백 패턴을 적용하여 덧셈, 곱셈 계산 기능 구현

--- src/main/java/springbook/learningtest/template/Calculator.java
+++ src/main/java/springbook/learningtest/template/Calculator.java
@@ -6,25 +6,19 @@ import java.io.IOException;
 
 public class Calculator {
 
-    public Integer calcSum(String filepath) throws IOException {
+    // 템플릿
+    public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback)
+        throws IOException {
         BufferedReader br = null;
-
         try {
-            // 한 줄씩 읽기 편하게 BufferedReader로 파일을 가져온다.
             br = new BufferedReader(new FileReader(filepath));
-            Integer sum = 0;
-            String line = null;
-
-            // 마지막 라인까지 한 줄씩 읽어가면서 숫 자를 더한다.
-            while ((line = br.readLine()) != null) {
-                sum += Integer.valueOf(line);
-            }
-
-            return sum;
+            int ret = callback.doSomethingWithReader(br);
+            return ret;
         } catch (IOException e) {
             System.out.println(e.getMessage());
             throw e;
         } finally {
+
             if (br != null) {
                 try {
                     br.close();
@@ -35,4 +29,37 @@ public class Calculator {
         }
     }
 
+    // 곱셈 계산 콜백
+    public Integer calcMultiply(String filepath) throws IOException {
+        BufferedReaderCallback multiplyCallback =
+            new BufferedReaderCallback() {
+                public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+                    Integer multiply = 1;
+                    String line = null;
+                    while ((line = br.readLine()) != null) {
+                        multiply *= Integer.valueOf(line);
+                    }
+                    return multiply;
+                }
+            };
+
+        return fileReadTemplate(filepath, multiplyCallback);
+    }
+
+    // 덧셈 계산 콜백
+    public Integer calcSum(String filepath) throws IOException {
+        BufferedReaderCallback sumCallback = new BufferedReaderCallback() {
+            @Override
+            public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+                Integer sum = 0;
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    sum += Integer.valueOf(line);
+                }
+                return sum;
+            }
+        };
+
+        return fileReadTemplate(filepath, sumCallback);
+    }
 }
