7f65d8a2a2dbd914c84e2b1ecec5bffe5426bea8
jujubebat <bbwwpark@naver.com>
2021-06-10 14:00:48 +0900

    refactor: PreparedStatement 전략을 익명 내부 클래스로 DI 하도록 수정

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -16,24 +16,26 @@ public class UserDao {
         this.dataSource = dataSource;
     }
 
-    public void add(User user) throws ClassNotFoundException, SQLException {
-        class AddStatement implements StatementStrategy {
-
-            public PreparedStatement makePreparedStatement(Connection c)
-                throws SQLException {
-                PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+    public void add(User user) throws SQLException {
+        jdbcContextWithStatementStrategy(new StatementStrategy() {
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values( ?,?,?)");
                 ps.setString(1, user.getId());
                 ps.setString(2, user.getName());
                 ps.setString(3, user.getPassword());
                 return ps;
             }
-        }
-
-        StatementStrategy st = new AddStatement();
-        jdbcContextWithStatementStrategy(st);
+        });
     }
 
-    public User get(String id) throws ClassNotFoundException, SQLException {
+    public void deleteAll() throws SQLException {
+        jdbcContextWithStatementStrategy( new StatementStrategy() {
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                return c.prepareStatement("delete from users"); }
+        } );
+    }
+
+    public User get(String id) throws SQLException {
         Connection c = dataSource.getConnection();
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 
@@ -59,13 +61,6 @@ public class UserDao {
         return user;
     }
 
-    // 아래 deleteAll() 메서드는 클라이언트다. 컨텍스트가 필요로 하는 전략의 특정 구현 클래스 오브젝를 만들어서 제공한다.(DI)
-    public void deleteAll() throws SQLException {
-        StatementStrategy st = new DeleteAllStatement(); // 선정한 전략 클래스의 오브젝트 생성
-        jdbcContextWithStatementStrategy(st); // 컨텍스트 호출. 전략 오브젝트 전달
-    }
-
-    // 메소드로 분리한 try/catch/finally 컨텍스트 코드. 인자로 전략을 받고있다.
     public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
         Connection c = null;
         PreparedStatement ps = null;
