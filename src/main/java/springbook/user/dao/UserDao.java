bdbec567638cc588dd015d869d621c6ae260e2bd
jujubebat <bbwwpark@naver.com>
2021-06-10 12:51:57 +0900

    refactor: 전략패턴 DI 적용을 위해 클라이언트와 컨텍스트 코드를 분리함

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -56,33 +56,28 @@ public class UserDao {
         return user;
     }
 
+    // 아래 deleteAll() 메서드는 클라이언트다. 컨텍스트가 필요로 하는 전략의 특정 구현 클래스 오브젝를 만들어서 제공한다.(DI)
     public void deleteAll() throws SQLException {
+        StatementStrategy st = new DeleteAllStatement(); // 선정한 전략 클래스의 오브젝트 생성
+        jdbcContextWithStatementStrategy(st); // 컨텍스트 호출. 전략 오브젝트 전달
+    }
+
+    // 메소드로 분리한 try/catch/finally 컨텍스트 코드. 인자로 전략을 받고있다.
+    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
         Connection c = null;
         PreparedStatement ps = null;
 
         try {
             c = dataSource.getConnection();
 
-            StatementStrategy strategy = new DeleteAllStatement();
-            ps = strategy.makePreparedStatement(c);
+            ps = stmt.makePreparedStatement(c);
 
             ps.executeUpdate();
         } catch (SQLException e) {
             throw e;
         } finally {
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                }
-            }
-
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                }
-            }
+            if (ps != null) { try { ps.close(); } catch (SQLException e) { } }
+            if (c != null) { try { c.close(); } catch (SQLException e) { } }
         }
     }
 
