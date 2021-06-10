d3152023d476e5d4a23ee051dfc0b7fca58af35e
jujubebat <bbwwpark@naver.com>
2021-06-10 23:38:09 +0900

    refactor: UserDao 인터페이스를 도입하여 DAO를 데이터 엑세스 기술에서 독립 시킴 - 중복 데이터 삽입시 어떤 예외가 던져지는지 확인하는 테스트 코드 추가 - 중복 데이터 삽입시 발생하는 SQLException이 DuplicateKeyException로 전환되는지 확인하는 테스트 코드 추가

--- src/test/java/springbook/user/dao/UserDaoTest.java
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -1,17 +1,20 @@
 package springbook.user.dao;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.sql.SQLException;
 import java.util.List;
+import javax.sql.DataSource;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import springbook.user.domain.User;
@@ -21,7 +24,9 @@ import springbook.user.domain.User;
 public class UserDaoTest {
 
     @Autowired
-    private UserDao dao;
+    private UserDaoJdbc dao;
+    @Autowired
+    private DataSource dataSource;
     private User user1;
     private User user2;
     private User user3;
@@ -34,7 +39,7 @@ public class UserDaoTest {
     }
 
     @Test
-    public void addAndGet() throws SQLException, ClassNotFoundException {
+    public void addAndGet() {
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
@@ -52,7 +57,7 @@ public class UserDaoTest {
     }
 
     @Test
-    public void count() throws SQLException, ClassNotFoundException {
+    public void count() {
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
@@ -67,8 +72,7 @@ public class UserDaoTest {
     }
 
     @Test(expected = EmptyResultDataAccessException.class)
-    public void getUserFailure() throws SQLException, ClassNotFoundException {
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+    public void getUserFailure() {
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
         dao.get("unknown_id");
@@ -83,14 +87,14 @@ public class UserDaoTest {
         List<User> users1 = dao.getAll();
         assertThat(users1.size(), is(1));
         checkSameUser(user1, users1.get(0));
-        dao.add(
-            user2); // Id: leegw700
+
+        dao.add(user2); // Id: leegw700
         List<User> users2 = dao.getAll();
         assertThat(users2.size(), is(2));
         checkSameUser(user1, users2.get(0));
         checkSameUser(user2, users2.get(1));
-        dao.add(
-            user3); // Id: bumjin
+
+        dao.add(user3); // Id: bumjin
         List<User> users3 = dao.getAll();
         assertThat(users3.size(), is(3));
         checkSameUser(user3, users3.get(0));
@@ -103,5 +107,26 @@ public class UserDaoTest {
         assertThat(user1.getName(), is(user2.getName()));
         assertThat(user1.getPassword(), is(user2.getPassword()));
     }
+
+    @Test(expected = DuplicateKeyException.class)
+    public void duplicateKey() {
+        dao.deleteAll();
+        dao.add(user1);
+        dao.add(user1);
+    }
+
+    @Test
+    public void sqlExceptionTranslate() {
+        dao.deleteAll();
+        try {
+            dao.add(user1);
+            dao.add(user1);
+        } catch (DuplicateKeyException ex) {
+            SQLException sqlException = (SQLException) ex.getRootCause();
+            SQLExceptionTranslator set = new SQLErrorCodeSQLExceptionTranslator(this.dataSource);
+            assertThat(set.translate(null, null, sqlException), instanceOf(DuplicateKeyException.class));
+        }
+    }
+
 
 }
