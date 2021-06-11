ea5fd604efd38e547856fbba9c2e20f1f3c7fd72
jujubebat <bbwwpark@naver.com>
2021-06-11 19:17:58 +0900

    feat: 사용자 레벨 업데이트시 이를 사용자에게 메일로 알려주는 기능 구현

--- src/main/java/springbook/user/dao/UserDaoJdbc.java
+++ src/main/java/springbook/user/dao/UserDaoJdbc.java
@@ -17,6 +17,7 @@ public class UserDaoJdbc implements UserDao {
             user.setId(rs.getString("id"));
             user.setName(rs.getString("name"));
             user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
             user.setLevel(Level.valueOf(rs.getInt("level")));
             user.setLogin(rs.getInt("login"));
             user.setRecommend(rs.getInt("recommend"));
@@ -31,10 +32,11 @@ public class UserDaoJdbc implements UserDao {
     }
 
     public void add(final User user) {
-        this.jdbcTemplate.update("insert into users(id, name, password, level, login, recommend) "
-                + "values(?,?,?,?,?,?)", user.getId(), user.getName(),
-            user.getPassword(), user.getLevel().intValue(),
-            user.getLogin(), user.getRecommend());
+        this.jdbcTemplate
+            .update("insert into users(id, name, password, email, level, login, recommend) "
+                    + "values(?,?,?,?,?,?,?)", user.getId(), user.getName(),
+                user.getPassword(), user.getEmail(), user.getLevel().intValue(),
+                user.getLogin(), user.getRecommend());
     }
 
     public void deleteAll() {
@@ -42,7 +44,8 @@ public class UserDaoJdbc implements UserDao {
     }
 
     public User get(String id) {
-        return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[]{id}, this.userMapper);
+        return this.jdbcTemplate
+            .queryForObject("select * from users where id = ?", new Object[]{id}, this.userMapper);
     }
 
     public List<User> getAll() {
@@ -54,8 +57,8 @@ public class UserDaoJdbc implements UserDao {
     }
 
     public void update(User user) {
-        this.jdbcTemplate.update("update users set name = ?, password = ?, level = ?, login = ?, "
-                + "recommend = ? where id = ? ", user.getName(), user.getPassword(),
+        this.jdbcTemplate.update("update users set name = ?, password = ?, email = ? ,level = ?, login = ?, "
+                + "recommend = ? where id = ? ", user.getName(), user.getPassword(), user.getEmail(),
             user.getLevel().intValue(), user.getLogin(), user.getRecommend(), user.getId());
     }
 }
--- src/main/java/springbook/user/domain/User.java
+++ src/main/java/springbook/user/domain/User.java
@@ -5,14 +5,17 @@ public class User {
     String id;
     String name;
     String password;
+    String email;
     Level level;
     int login;
     int recommend;
 
-    public User(String id, String name, String password, Level level, int login, int recommend) {
+    public User(String id, String name, String password, String email,
+        Level level, int login, int recommend) {
         this.id = id;
         this.name = name;
         this.password = password;
+        this.email = email;
         this.level = level;
         this.login = login;
         this.recommend = recommend;
@@ -28,6 +31,14 @@ public class User {
         } else {
             this.level = nextLevel;
         }
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public Level getLevel() {
