c814e82a7d16562d37d14037167d0b0f5014ee6a
jujubebat <bbwwpark@naver.com>
2021-06-11 20:46:34 +0900

    test: 목 오브젝트를 활요한 테스트 코드 추가 - 목 오브젝트를 활용하여 upgradeLevel 메서드 실행시 User객체를 SimpleMailMessage 객체에게 올바르게 전달하는지 테스트하는 코드를 구현함

--- src/test/java/springbook/user/service/UserServiceTest.java
+++ src/test/java/springbook/user/service/UserServiceTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.fail;
 import static springbook.user.service.UserService.MIN_LOGCOUNT_FOR_SILVER;
 import static springbook.user.service.UserService.MIN_RECCOMEND_FOR_GOLD;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import javax.sql.DataSource;
@@ -13,7 +14,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -40,28 +44,45 @@ public class UserServiceTest {
     @Before
     public void setUp() {
         users = Arrays.asList(
-            new User("bumjin", "박범진", "p1", "bumjin@naver.com" ,Level.BASIC, MIN_LOGCOUNT_FOR_SILVER - 1, 0),
-            new User("joytouch", "강명성", "p2", "joytouch@naver.com",Level.BASIC, MIN_LOGCOUNT_FOR_SILVER, 0),
-            new User("erwins", "신승한", "p3", "erwins@naver.com",Level.SILVER, 60, MIN_RECCOMEND_FOR_GOLD - 1),
-            new User("madnite1", "이상호", "p4","madnite1@naver.com" ,Level.SILVER, 60, MIN_RECCOMEND_FOR_GOLD),
-            new User("green", "오민규", "p5","green@naver.com" ,Level.GOLD, 100, Integer.MAX_VALUE)
+            new User("bumjin", "박범진", "p1", "bumjin@naver.com", Level.BASIC,
+                MIN_LOGCOUNT_FOR_SILVER - 1, 0),
+            new User("joytouch", "강명성", "p2", "joytouch@naver.com", Level.BASIC,
+                MIN_LOGCOUNT_FOR_SILVER, 0),
+            new User("erwins", "신승한", "p3", "erwins@naver.com", Level.SILVER, 60,
+                MIN_RECCOMEND_FOR_GOLD - 1),
+            new User("madnite1", "이상호", "p4", "madnite1@naver.com", Level.SILVER, 60,
+                MIN_RECCOMEND_FOR_GOLD),
+            new User("green", "오민규", "p5", "green@naver.com", Level.GOLD, 100, Integer.MAX_VALUE)
         );
     }
 
     // 각 사용자별로 업그레이드 후의 예상 레벨을 검증한다.
     @Test
+    @DirtiesContext // 컨텍스트의 DI 설정을 변경하는 테스트라는 것을 알려준다.
     public void upgradeLevels() throws Exception {
         userDao.deleteAll();
         for (User user : users) {
             userDao.add(user);
         }
+
+        // 메일 발송 결과를 테스트할 수 있도록 목 오브젝트를 만들어 userService의 의존 오브젝트로 주입해준다.
+        MockMailSender mockMailSender = new MockMailSender();
+        userService.setMailSender(mockMailSender);
+
         userService.upgradeLevels();
         checkLevelUpgraded(users.get(0), false);
         checkLevelUpgraded(users.get(1), true);
         checkLevelUpgraded(users.get(2), false);
         checkLevelUpgraded(users.get(3), true);
         checkLevelUpgraded(users.get(4), false);
+
+        // 목 오브젝트에 저장된 메일 수신자 목록을 가져와 업그레이드 대상과 일치하는지 확인한다.
+        List<String> request = mockMailSender.getRequests();
+        assertThat(request.size(), is(2));
+        assertThat(request.get(0), is(users.get(1).getEmail()));
+        assertThat(request.get(1), is(users.get(3).getEmail()));
     }
+
 
     // 어떤 레벨로 바뀔 것인가가 아니라, 다음 레벨로 업 그레이드될 것인가 아닌가를 체크한다.
     private void checkLevelUpgraded(User user, boolean upgraded) {
@@ -132,6 +153,24 @@ public class UserServiceTest {
 
     static class TestUserServiceException extends RuntimeException {
 
+    }
+
+    static class MockMailSender implements MailSender {
+
+        // UserService로부터 전송 요청 을 받은 메일 주소를 저장해두고 이를 읽을수있게 한다
+        private List<String> requests = new ArrayList<String>();
+
+        public List<String> getRequests() {
+            return requests;
+        }
+
+        // 전송 요청을 받은 이메일 주소를 저장해둔다. 간단하게 첫 번째 수신자 메일 주소만 저장한다.
+        public void send(SimpleMailMessage mailMessage) throws MailException {
+            requests.add(mailMessage.getTo()[0]);
+        }
+
+        public void send(SimpleMailMessage[] mailMessage) throws MailException {
+        }
     }
 
 }
